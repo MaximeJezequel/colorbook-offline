bbf91d4dcdd89b7729fce9577a1a4c47b2ca7cfc
Maxime Jezequel <maxime.jezequel@gmail.com>
2024-01-02 16:19:59 +0100

    Add credit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,22 @@ import esp from "../assets/esp.svg";
 import rocket from "../assets/rocket.svg";
 import monalisa from "../assets/monalisa_thick.svg";
 
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-cards";
 
 export const Home = ({ setChoice }: { setChoice: any }) => {
+  const [isMessageVisible, setMessageVisible] = useState(false);
+
+  const showMessage = () => {
+    setMessageVisible(true);
+    setTimeout(() => {
+      setMessageVisible(false);
+    }, 3000);
+  };
+
   return (
     <div className="home">
       <div className="home-container">
@@ -43,7 +53,10 @@ export const Home = ({ setChoice }: { setChoice: any }) => {
           </Swiper>
         </div>
         <div className="home-credit">
-          <img src={info} alt="Credit : @emji_magie" />
+          {isMessageVisible && (
+            <div className="credit-message"><a href="https:www.instagram.com/emji.magie?igsh=N2thbGIuNzZ6bnBu" target="_blank">Credit @emji_magie</a></div>
+            )}
+            <img src={info} alt="Credit : @emji_magie" onClick={showMessage} />
         </div>
       </div>
     </div>
